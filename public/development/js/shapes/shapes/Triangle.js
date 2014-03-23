845909c7a36050daa30321d95779a861e1a3cf3d
Antoine Cayphas <acayphas@ulb.ac.be>
2014-03-23 19:29:05 +0100

    preRender
    Creation de la methode PreRender pour les 3 formes

--- public/development/js/shapes/shapes/Triangle.js
+++ public/development/js/shapes/shapes/Triangle.js
@@ -14,6 +14,32 @@ define([
 
 	}
 
+	Triangle.prototype.preRender = function () {
+		var context = this.canvas.getContext('2d');
+		var h = side * (Math.sqrt(3)/2);
+			
+		context.strokeStyle = "#ff0000";
+			
+		context.save();
+		context.translate(this.x, this.y);
+		
+		context.beginPath();
+			
+			context.moveTo(0, -h / 2);
+			context.lineTo( -this.size / 2, h / 2);
+			context.lineTo(this.size / 2, h / 2);
+			context.lineTo(0, -h / 2);
+	
+			context.fillStyle = 'yellow';
+			context.fill();
+			context.lineWidth = 7;
+			context.strokeStyle = 'black';
+			context.stroke();
+			
+		context.closePath();
+		context.save();
+	}
+
 	return Triangle;
 
 });
